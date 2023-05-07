ac7a582a3e0bdf05b5bf767cbd9e840db295e1e0
iamrishupatel <rishupatel04@gmail.com>
2023-05-07 08:53:09 +0530

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+	$color: #7869d7;
+	h1 {
+		color: $color;
+	}
+</style>
